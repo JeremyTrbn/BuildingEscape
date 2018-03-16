491fe647003b3d8b08ef3e4198452d69ad8192cc
Jeremy <jeremy.tourbillon@epitech.eu>
2018-03-16 14:53:24 +0100

    Add files via upload

---
+++ OpenDoor.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+#include "CoreMinimal.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+void UOpenDoor::OpenDoor()
+{
+    Owner->SetActorRotation(FRotator(0.f, OpenAngle, 0.f));
+}
+
+void UOpenDoor::CloseDoor()
+{
+    Owner->SetActorRotation(FRotator(0.f, 0.f, 0.f));
+}
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+    
+    Owner = GetOwner();
+    ActorThatOpen = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    
+    if (PressurePlate->IsOverlappingActor(ActorThatOpen))
+    {
+        OpenDoor();
+        UE_LOG(LogTemp, Display, TEXT("The Door is open"));
+        LastDoorOpenTime = GetWorld()->GetTimeSeconds();
+    }
+    if (GetWorld()->GetTimeSeconds() - LastDoorOpenTime > DoorCloseDelay)
+    {
+        CloseDoor();
+    }
+}
+
